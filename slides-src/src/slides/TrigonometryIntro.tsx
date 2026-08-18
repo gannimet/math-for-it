e94417b72e400d780b2357f9400f7fd0e2d66182
Richard Wotzlaw <r.wotzlaw@gmail.com>
2026-08-18 16:47:04 +0200

    animated sine curve

--- slides-src/src/slides/TrigonometryIntro.tsx
+++ slides-src/src/slides/TrigonometryIntro.tsx
@@ -9,6 +9,7 @@ import {
     Point,
     Theme,
     useMovablePoint,
+    useStopwatch,
     vec,
 } from 'mafs';
 import { useEffect, useState } from 'react';
@@ -34,6 +35,26 @@ export default function TrigonometryIntro() {
         setAngle(newAngle);
     }, [trigAnchor.x, trigAnchor.y]);
 
+    const { time, start, stop } = useStopwatch();
+    const [isSineAnimationRunning, setIsSineAnimationRunning] = useState(false);
+
+    const toggleSineAnimation = () => {
+        if (isSineAnimationRunning) {
+            stop();
+            setIsSineAnimationRunning(false);
+        } else {
+            start();
+            setIsSineAnimationRunning(true);
+        }
+    };
+
+    useEffect(() => {
+        const newAngle = time % (2 * Math.PI);
+
+        setAngle(newAngle);
+        trigAnchor.setPoint([Math.cos(newAngle), Math.sin(newAngle)]);
+    }, [time]);
+
     return (
         <>
             <Slide data-auto-animate>
@@ -42,14 +63,14 @@ export default function TrigonometryIntro() {
                 <div className="r-stretch" style={{ position: 'relative' }}>
                     <Mafs
                         height={700}
-                        viewBox={{ x: [-3.5, 3.2], y: [-1, 1.2] }}
+                        viewBox={{ x: [-3.5, 3], y: [-1, 1] }}
                         pan={false}
                         zoom={false}
                     >
                         <g data-id="trig-coords">
                             <Coordinates.Cartesian
-                                xAxis={{ labels: false }}
-                                yAxis={{ labels: false }}
+                                xAxis={{ labels: (l) => (Math.abs(l) === 1 ? l : '') }}
+                                yAxis={{ labels: (l) => (Math.abs(l) === 1 ? l : '') }}
                             />
                         </g>
 
@@ -77,9 +98,6 @@ export default function TrigonometryIntro() {
                             />
                         </g>
 
-                        <g data-id="trig-radius-hint">
-                            <LaTeX tex={String.raw`r=1`} at={[0, 1.3]} />
-                        </g>
                         <g data-id="trig-angle-value">
                             <LaTeX
                                 tex={String.raw`\alpha = ${((180 * angle) / Math.PI).toFixed(1)}^{\circ}`}
@@ -164,73 +182,126 @@ export default function TrigonometryIntro() {
                 <h2>Trigonometrie</h2>
 
                 <div className="r-stretch">
-                    <Mafs
-                        height={700}
-                        viewBox={{ x: [-3.5, 3.2], y: [-1, 1.2] }}
-                        pan={false}
-                        zoom={false}
-                    >
-                        <g data-id="trig-coords">
-                            <Coordinates.Cartesian
-                                xAxis={{ labels: false }}
-                                yAxis={{ labels: false }}
-                            />
-                        </g>
+                    <div style={{ position: 'relative' }}>
+                        <Mafs
+                            height={700}
+                            viewBox={{ x: [-3.5, 6.5], y: [-1, 1.2] }}
+                            pan={false}
+                            zoom={false}
+                        >
+                            <g data-id="trig-coords">
+                                <Coordinates.Cartesian
+                                    xAxis={{ labels: (l) => (l > 0 ? l : '') }}
+                                    yAxis={{ labels: (l) => (Math.abs(l) === 1 ? l : '') }}
+                                />
+                            </g>
 
-                        <g data-id="trig-circle">
-                            <Circle center={[-2, 0]} radius={1} weight={3} />
-                        </g>
-                        <g data-id="trig-anchor-radius">
-                            <Line.Segment
-                                point1={[-2, 0]}
-                                point2={[trigAnchor.x - 2, trigAnchor.y]}
-                                color={Theme.pink}
-                                weight={3}
-                            />
-                        </g>
+                            <g data-id="trig-circle">
+                                <Circle center={[-2, 0]} radius={1} weight={3} />
+                            </g>
+                            <g data-id="trig-anchor-radius">
+                                <Line.Segment
+                                    point1={[-2, 0]}
+                                    point2={[trigAnchor.x - 2, trigAnchor.y]}
+                                    color={Theme.pink}
+                                    weight={3}
+                                />
+                            </g>
 
-                        <g data-id="trig-angle-arc">
-                            <Plot.Parametric
-                                domain={[0, angle]}
-                                xy={(t) => [
-                                    Math.cos(t) * ANGLE_ARC_DIST - 2,
-                                    Math.sin(t) * ANGLE_ARC_DIST,
-                                ]}
-                                color={Theme.pink}
-                                weight={3}
-                            />
-                        </g>
+                            <g data-id="trig-angle-arc">
+                                <Plot.Parametric
+                                    domain={[0, angle]}
+                                    xy={(t) => [
+                                        Math.cos(t) * ANGLE_ARC_DIST - 2,
+                                        Math.sin(t) * ANGLE_ARC_DIST,
+                                    ]}
+                                    color={Theme.pink}
+                                    weight={3}
+                                />
+                            </g>
 
-                        <g style={{ fontSize: '0.6em' }} data-id="trig-alpha">
-                            <LaTeX tex={String.raw`\alpha`} at={[-1.65, 0.1]} color={Theme.pink} />
-                        </g>
+                            <g style={{ fontSize: '0.5em' }} data-id="trig-alpha">
+                                <LaTeX
+                                    tex={String.raw`\alpha`}
+                                    at={[-1.65, 0.1]}
+                                    color={Theme.pink}
+                                />
+                            </g>
 
-                        <g data-id="trig-sin-line">
-                            <Line.Segment
-                                point1={[trigAnchor.x - 2 + BBOX_EPSILON, 0]}
-                                point2={[trigAnchor.x - 2, trigAnchor.y]}
+                            <g data-id="trig-sin-line">
+                                <Line.Segment
+                                    point1={[trigAnchor.x - 2 + BBOX_EPSILON, 0]}
+                                    point2={[trigAnchor.x - 2, trigAnchor.y]}
+                                    color={Theme.green}
+                                    weight={4}
+                                />
+                            </g>
+                            <g data-id="trig-cos-line">
+                                <Line.Segment
+                                    point1={[-2, 0]}
+                                    point2={[trigAnchor.x - 2, BBOX_EPSILON]}
+                                    color={Theme.blue}
+                                    weight={4}
+                                />
+                            </g>
+
+                            <g data-id="trig-sin-graph">
+                                <Plot.OfX
+                                    y={(x) => Math.sin(x)}
+                                    color={Theme.green}
+                                    domain={[0, 10]}
+                                    weight={4}
+                                />
+                                <Plot.OfX
+                                    y={(x) => Math.cos(x)}
+                                    color={Theme.blue}
+                                    domain={[0, 10]}
+                                    weight={4}
+                                />
+
+                                <Plot.OfX
+                                    y={(x) => Math.sin(x)}
+                                    color={Theme.green}
+                                    domain={[-5, 0]}
+                                    weight={0.8}
+                                />
+                                <Plot.OfX
+                                    y={(x) => Math.cos(x)}
+                                    color={Theme.blue}
+                                    domain={[-5, 0]}
+                                    weight={0.8}
+                                />
+                            </g>
+
+                            <g data-id="trig-anchor-non-movable">
+                                <Point x={trigAnchor.x - 2} y={trigAnchor.y} color={Theme.pink} />
+                            </g>
+
+                            <Point
+                                x={time % (2 * Math.PI)}
+                                y={Math.sin(time)}
                                 color={Theme.green}
-                                weight={4}
                             />
-                        </g>
-                        <g data-id="trig-cos-line">
                             <Line.Segment
-                                point1={[-2, 0]}
-                                point2={[trigAnchor.x - 2, BBOX_EPSILON]}
-                                color={Theme.blue}
-                                weight={4}
+                                point1={[trigAnchor.x - 2, trigAnchor.y]}
+                                point2={[time % (2 * Math.PI), Math.sin(time)]}
+                                style="dashed"
+                                color={Theme.green}
                             />
-                        </g>
+                        </Mafs>
 
-                        <g data-id="trig-sin-graph">
-                            <Plot.OfX y={(x) => Math.sin(x)} color={Theme.green} />
-                            <Plot.OfX y={(x) => Math.cos(x)} color={Theme.blue} />
-                        </g>
-
-                        <g data-id="trig-anchor-non-movable">
-                            <Point x={trigAnchor.x - 2} y={trigAnchor.y} color={Theme.pink} />
-                        </g>
-                    </Mafs>
+                        <button
+                            style={{
+                                position: 'absolute',
+                                bottom: '20px',
+                                left: '20px',
+                                fontSize: '30px',
+                            }}
+                            onClick={() => toggleSineAnimation()}
+                        >
+                            {isSineAnimationRunning ? 'Stop' : 'Start'}
+                        </button>
+                    </div>
                 </div>
             </Slide>
         </>
